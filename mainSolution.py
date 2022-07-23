433a570335a9e4e012853214d2afbb1181bf8bd5
yangqiqii <109862099+yangqiqii@users.noreply.github.com>
2022-07-24 01:11:37 +0800

    Add files via upload

---
+++ mainSolution.py
@@ -0,0 +1,17 @@
+print("Hello World! This is our Team's solution!")
+
+def add():
+    print("This is function add")
+    return
+
+def delete():
+    print("This is function delete")
+    return
+
+def multiply():
+    print("This is function multiply")
+    return
+
+add()
+delete()
+multiply()
