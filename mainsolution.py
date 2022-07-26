f24d0f0d29b43b0655d92efb573b5f37f529e87c
Prog4sch <109902777+Prog4sch@users.noreply.github.com>
2022-07-26 21:23:42 +0800

    Add files via upload

---
+++ mainsolution.py
@@ -0,0 +1,17 @@
+print("Hello World! This is our Team's solution")
+
+def add():
+    print("This is our function add")
+    return
+
+def delete():
+    print("This is our function subtract")
+    return
+
+def multiply():
+    print("This is our function multiply")
+    return
+
+add()
+delete()
+multiply()
