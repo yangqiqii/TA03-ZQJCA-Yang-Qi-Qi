c16262554e1a33812c7e8cd0558cb35c509146af
yangqiqii <s10243334@connect.np.edu.sg>
2022-07-28 23:07:45 +0800

    mainsolution

--- mainsolution.py
+++ mainsolution.py
@@ -1,17 +1,5 @@
-print("Hello World! This is our Team's solution")
-
-def add():
-    print("This is our function add")
-    return
-
-def delete():
-    print("This is our function subtract")
-    return
-
-def multiply():
-    print("This is our function multiply")
-    return
-
-add()
-delete()
-multiply()
+import api, overheads, profit_loss, cash_on_hand
+def mainSolution():
+    """
+    """
+    
