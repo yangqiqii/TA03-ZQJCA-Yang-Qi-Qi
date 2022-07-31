de482b03a817c81ce86ee38e51a9471f91899eb8
yangqiqii <s10243334@connect.np.edu.sg>
2022-07-31 22:59:44 +0800

    updated code

--- api.py
+++ api.py
@@ -1,6 +1,5 @@
 import requests
 import json
-
 api_key = "RGC6YA5ZJFKM6OPR"
 url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=demo{api_key}"
 response = requests.get(url)
@@ -14,5 +13,13 @@ def api():
     Function does not require parameter 
     Function returns real time currency conversion rate from USD to SGD
     """
-    print(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}")
-api()
+    # Assign 
+    api_key = "RGC6YA5ZJFKM6OPR"
+    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=demo{api_key}"
+    response = requests.get(url)
+    data = response.json()
+    # Use data.keys() to 
+    #json.dumps(data["Realtime Currency Exchange Rate"], indent = 4)
+    forex = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+    return f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}"
+print(api())
