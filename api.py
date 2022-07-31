df84508ac5b625fcd47ba12eb6b3dbbaa16bea31
yangqiqii <s10243334@connect.np.edu.sg>
2022-07-31 23:04:25 +0800

    updated code

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
+# Import modules to run code
 import requests
 import json
+
+# Assign api key to the variaable "api_key"
 api_key = "RGC6YA5ZJFKM6OPR"
 url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=demo{api_key}"
 response = requests.get(url)
@@ -7,6 +10,7 @@ data = response.json()
 #data.keys()
 #json.dumps(data["Realtime Currency Exchange Rate"], indent = 4)
 forex = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+
 
 def api():
     """
