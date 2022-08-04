d41e8f48103267ea7ed53e1d90b185ee86da4f07
yangqiqii <109862099+yangqiqii@users.noreply.github.com>
2022-08-04 21:22:36 +0800

    Update mainsolution.py

--- mainsolution.py
+++ mainsolution.py
@@ -12,9 +12,9 @@ def mainSolution():
     # File path for summary_report.txt_file
     # Assign the file path to the variable "summary_file"
     summary_file = Path.cwd()/"summary_report.txt"
-    # Use .touch() to creatae the summary_report.txt file
+    # Use .touch() to create the summary_report.txt file
     summary_file.touch()
-    # Create "cohinder" to represent the index position of the items in the output  of cash_on_hand.cash_on_hand()
+    # Create "cohindex" to represent the index position of the items in the output of cash_on_hand.cash_on_hand()
     cohindex = 0
     # Create "plindex" to represent the index position of the items in the output of the profit_loss.profit_loss()
     plindex = 0
@@ -27,19 +27,19 @@ def mainSolution():
         # Use .writelines() to write the output of overheads.overheads() into summary_report.txt file
         # Use \n to add the item into a new line
         file.writelines(f" \n{overheads.overheads()}")
-        # Creating a while loop to repeat appending summary statement into summary_report.txt file for cohindex lesser than the number of items in the output of cash_on_hand.cash_on_hand()
+        # Create a while loop to repeat appending summary statement into summary_report.txt file for cohindex lesser than the number of items in the output of cash_on_hand.cash_on_hand()
         while cohindex < len(cash_on_hand.cash_on_hand()):
             # Use .writeline() to write the output of cash_on_hand.cash_on_hand() into summary_report.txt file
             # Use \n to add the items into a new line
             file.writelines(f" \n{cash_on_hand.cash_on_hand()[cohindex]}")
             # To increase index position by 1
             cohindex += 1
-        # Creating while loop to repeat appending summary statement into summary_report.txt file fot plindex lesser than the number of  items in the output of profit_loss.profit_loss()
+        # Create a while loop to repeat appending summary statement into summary_report.txt file fot plindex lesser than the number of items in the output of profit_loss.profit_loss()
         while plindex < len(profit_loss.profit_loss()):
             # Use .writeline() to write the output of profit_loss.profit_loss() into summary_report.txt file
             # Use \n to add the items into a new line
             file.writelines(f"\n{profit_loss.profit_loss()[plindex]}")
-            # To increase index position by 1
+            # Increase index position by 1
             plindex += 1
 # Execute the function
 mainSolution()
