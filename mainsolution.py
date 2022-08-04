5906ed94d45933240610899e07f572ee0dce48df
amanda <109902721+amandangyx@users.noreply.github.com>
2022-08-04 13:26:49 +0800

    Update mainsolution.py

--- mainsolution.py
+++ mainsolution.py
@@ -1,12 +1,24 @@
+# Import modules to run code
 from pathlib import Path
 import api, overheads, cash_on_hand, profit_loss
+
+# Define the function mainSolution()
 def mainSolution():
+    # Documentation of the function mainSolution()
     """
+    Function does not require parameter
+    Function returns summary report for currency exchange rate, overheads, cash on hand and profit and loss
     """
+    # File path for summary_report.txt_file
+    # Assign the file path to the variable "summary_file"
     summary_file = Path.cwd()/"summary_report.txt"
+    # Use .touch() to creatae the summary_report.txt file
     summary_file.touch()
+    # Create "cohinder" to represent the index position of the items in the output  of cash_on_hand.cash_on_hand()
     cohindex = 0
+    # Create "plindex" to represent the index position of the items in the output of the profit_loss.profit_loss()
     plindex = 0
+    # Open summary_report.txt file for appending
     cohsummary = cash_on_hand.cash_on_hand()
     plsummary = profit_loss.profit_loss()
     with summary_file.open(mode = "w", encoding = "UTF-8") as file:
