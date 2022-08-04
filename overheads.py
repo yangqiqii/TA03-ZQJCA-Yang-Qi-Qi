a3c463690f2916389c3a3dc1359efe7d18d50800
yangqiqii <s10243334@connect.np.edu.sg>
2022-08-04 21:45:10 +0800

    FINAL

--- overheads.py
+++ overheads.py
@@ -10,11 +10,7 @@ def overheads():
     Function does not require parameter
     Function returns the highest overhead category and its value.
     """
-<<<<<<< HEAD
     # Create an empty dictionary to store category(key) : values(value)
-=======
-    # Create an empty dictionary to store catedory(key) : values(value)
->>>>>>> 0d475fd20a385b6d1a625fd0cb0c669e63fa66c5
     dictionary = {}
     # Create an empty list to store the values of each category
     value_list = []
@@ -23,17 +19,10 @@ def overheads():
     overheads = Path.cwd()/"csv_reports"/"overheads-day-45.csv"
     # Open overheads-day-45.csv for reading
     with overheads.open(mode = "r", encoding = "UTF-8") as file:
-<<<<<<< HEAD
         # Use csv.reader() to read csv file
         # Assign csv.reader(file) to the variable "reader"
         reader = csv.reader(file)
         # Use next() to skip the header
-=======
-        # Use csv.reader() to to read csv file
-        # Assign csv.reader (file) to the variable "reader"
-        reader = csv.reader(file)
-        # Using next() to skip the header
->>>>>>> 0d475fd20a385b6d1a625fd0cb0c669e63fa66c5
         next(reader)
         # Access the sub-list in reader
         for datas in reader:
@@ -47,23 +36,12 @@ def overheads():
             # Append "valuedata" into value_list
             value_list.append(valuedata)
         # Use .items() to return a tuple of key-value within a special type of list
-<<<<<<< HEAD
         # cateogry(value) : values(value)
-=======
-        # caterogy(value) : values(value)
->>>>>>> 0d475fd20a385b6d1a625fd0cb0c669e63fa66c5
         for category, values in dictionary.items():
             # Use max() to find the highest value in value_list
             # Create a condition when values is the same as the highest value in value_list
             if values == max(value_list):
-<<<<<<< HEAD
                 # Return summary statement for overheaads
                 return f"[HIGHEST OVERHEADS] {category.upper()}: SGD{round(max(value_list)*api.forex, 2)}"
 # Execute the function
 print(overheads())
-=======
-                # Return summary statement for overheads
-                return f"[HIGHEST OVERHEADS] {category.upper()}: SGD{round(max(value_list)*api.forex, 2)}"
-# Execute the function
-print(overheads())
->>>>>>> 0d475fd20a385b6d1a625fd0cb0c669e63fa66c5
