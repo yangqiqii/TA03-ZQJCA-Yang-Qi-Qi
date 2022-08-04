b69ff8bfe0259b8a38f3039cba442426247de4a7
yangqiqii <s10243334@connect.np.edu.sg>
2022-08-04 21:44:34 +0800

    FINAL

--- mainsolution.py
+++ mainsolution.py
@@ -7,28 +7,16 @@ def mainSolution():
     # Documentation of the function mainSolution()
     """
     Function does not require parameter
-<<<<<<< HEAD
     Function returns summary report for currency exchange rate, overheads, cash on hand, and profit and loss
     """
     # File path for summary_report.txt file
-=======
-    Function returns summary report for currency exchange rate, overheads, cash on hand and profit and loss
-    """
-    # File path for summary_report.txt_file
->>>>>>> 0d475fd20a385b6d1a625fd0cb0c669e63fa66c5
     # Assign the file path to the variable "summary_file"
     summary_file = Path.cwd()/"summary_report.txt"
     # Use .touch() to create the summary_report.txt file
     summary_file.touch()
-<<<<<<< HEAD
     # Create "cohindex" to represent the index postion of the items in the output of cash_on_hand.cash_on_hand()
     cohindex = 0
     # Create "plindex" to represent the index position of the items in the output of profit_loss.profit_loss()
-=======
-    # Create "cohindex" to represent the index position of the items in the output of cash_on_hand.cash_on_hand()
-    cohindex = 0
-    # Create "plindex" to represent the index position of the items in the output of the profit_loss.profit_loss()
->>>>>>> 0d475fd20a385b6d1a625fd0cb0c669e63fa66c5
     plindex = 0
     # Open summary_report.txt file for writing
     with summary_file.open(mode = "w", encoding = "UTF-8") as file:
@@ -37,16 +25,11 @@ def mainSolution():
     # Open summary_report.txt file for appending
     with summary_file.open(mode = "a", encoding = "UTF-8") as file:
         # Use .writelines() to write the output of overheads.overheads() into summary_report.txt file
-<<<<<<< HEAD
         # Use \n to add the item to a new line
-=======
-        # Use \n to add the item into a new line
->>>>>>> 0d475fd20a385b6d1a625fd0cb0c669e63fa66c5
         file.writelines(f" \n{overheads.overheads()}")
         # Create a while loop to repeat appending summary statement into summary_report.txt file for cohindex lesser than the number of items in the output of cash_on_hand.cash_on_hand()
         while cohindex < len(cash_on_hand.cash_on_hand()):
             # Use .writeline() to write the output of cash_on_hand.cash_on_hand() into summary_report.txt file
-<<<<<<< HEAD
             # Use \n to add the item to a new line
             file.writelines(f"\n{cash_on_hand.cash_on_hand()[cohindex]}")
             # Increase index position by 1
@@ -60,18 +43,3 @@ def mainSolution():
             plindex += 1
 # Execute the function
 mainSolution()
-=======
-            # Use \n to add the items into a new line
-            file.writelines(f" \n{cash_on_hand.cash_on_hand()[cohindex]}")
-            # To increase index position by 1
-            cohindex += 1
-        # Create a while loop to repeat appending summary statement into summary_report.txt file fot plindex lesser than the number of items in the output of profit_loss.profit_loss()
-        while plindex < len(profit_loss.profit_loss()):
-            # Use .writeline() to write the output of profit_loss.profit_loss() into summary_report.txt file
-            # Use \n to add the items into a new line
-            file.writelines(f"\n{profit_loss.profit_loss()[plindex]}")
-            # Increase index position by 1
-            plindex += 1
-# Execute the function
-mainSolution()
->>>>>>> 0d475fd20a385b6d1a625fd0cb0c669e63fa66c5
